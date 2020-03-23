5d0691f5761174ed45436745a3b186db79a9018c
Taso <aniko17@freeuni.edu.ge>
2020-03-23 17:13:31 +0400

    created fields for one note, added style to it

--- src/components/Notes.js
+++ src/components/Notes.js
@@ -1,23 +1,54 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Checkbox } from 'react-bootstrap';
 
 const Notes = (props)  => {    
 
     return (
-        <div className="container" /*style={containerStyle}*/>
+        <div>
           {props.notes && props.notes.map((key, index) => 
-          <>
-            <p /*style={noteStyle}*/>{key}</p>
-            <button id="delete" type="button"
-             className="btn btn-danger"
-              onClick={props.delete.bind(this, key)} /*style={btnStyle}*/>X</button>
-          </>
+
+          <div className="container" style={containerStyle}>
+            <div style = {noteStyle} className="input-group mb-3">
+              <div className="input-group-prepend">
+                <div className="input-group-text">
+                  <input type="checkbox" aria-label="Checkbox for following text input"/>
+                </div>
+              </div>
+              <input type="text" class="form-control" aria-label="Text input with checkbox" 
+                     readOnly value={key}/>
+            </div>
+            <button id="delete"
+                    type="button"
+                    className="btn btn-danger"
+                    onClick={props.delete.bind(this, key)}
+                    style={btnStyle}>X</button>
+          </div>
           )}
+
         </div>
     );
   }
 
+  const noteStyle = {
+    marginTop: 20,
+    width: '100%', 
+    right: 15
+  }
 
+  const containerStyle = {
+      display: 'flex',
+      position: 'relative',
+      flexDirection: 'column'
+  }
+
+  const btnStyle = {
+    width: 40,
+    height: 40,
+    borderRadius: 35,
+    textAlign: 'center',
+    position: 'absolute',
+    right: 0,
+    top: 18
+  }
   
   export default Notes;
