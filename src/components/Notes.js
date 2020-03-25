e004f63b51250aa69e648b8203e6e8d11129ef88
Taso <aniko17@freeuni.edu.ge>
2020-03-25 11:54:48 +0400

    added uniqe keys to list elements

--- src/components/Notes.js
+++ src/components/Notes.js
@@ -1,55 +1,66 @@
-import React from 'react';
+import React from "react";
+import uniqid from "uniqid";
 
-const Notes = (props)  => {    
-
-    return (
-        <div>
-          {props.notes && props.notes.map((key, index) => 
-
-          <div className="container" style={containerStyle} key={index}>
-            <div style = {noteStyle} className="input-group mb-3">
+const Notes = props => {
+  return (
+    <div>
+      {props.notes &&
+        props.notes.map(key => (
+          <div className="container" style={containerStyle} key={uniqid()}>
+            <div style={noteStyle} className="input-group mb-3">
               <div className="input-group-prepend">
                 <div className="input-group-text">
-                  <input type="checkbox" aria-label="Checkbox for following text input"/>
+                  <input
+                    type="checkbox"
+                    aria-label="Checkbox for following text input"
+                  />
                 </div>
               </div>
-              <input type="text" className="form-control" aria-label="Text input with checkbox" 
-                     readOnly value={key}/>
+              <input
+                type="text"
+                className="form-control"
+                aria-label="Text input with checkbox"
+                readOnly
+                value={key}
+              />
             </div>
-            <button id="delete"
-                    type="button"
-                    className="btn btn-danger"
-                    onClick={() => props.delete(key)}
-                    style={btnStyle}>X</button>
+            <button
+              id="delete"
+              type="button"
+              className="btn btn-danger"
+              onClick={() => props.delete(key)}
+              style={btnStyle}
+            >
+              X
+            </button>
           </div>
-          )}
+        ))}
+    </div>
+  );
+};
 
-        </div>
-    );
-  }
+const noteStyle = {
+  marginTop: 20,
+  width: "100%",
+  zIndex: 1,
+  right: 15
+};
 
-  const noteStyle = {
-    marginTop: 20,
-    width: '100%', 
-    zIndex: 1,
-    right: 15
-  }
+const containerStyle = {
+  display: "flex",
+  position: "relative",
+  flexDirection: "column"
+};
 
-  const containerStyle = {
-      display: 'flex',
-      position: 'relative',
-      flexDirection: 'column'
-  }
+const btnStyle = {
+  width: 40,
+  height: 40,
+  borderRadius: 35,
+  textAlign: "center",
+  position: "absolute",
+  zIndex: 2,
+  right: 0,
+  top: 18
+};
 
-  const btnStyle = {
-    width: 40,
-    height: 40,
-    borderRadius: 35,
-    textAlign: 'center',
-    position: 'absolute',
-    zIndex: 2,
-    right: 0,
-    top: 18
-  }
-  
-  export default Notes;
+export default Notes;
